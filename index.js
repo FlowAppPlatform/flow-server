15b9da5b1d788a354f842306bc1a2fe7c49e8b0d
Jerry Edebua <jeredebua@gmail.com>
2018-10-12 16:58:07 +0300

    added endpoint to execute graphs

--- index.js
+++ index.js
@@ -10,5 +10,15 @@ app.get('/', (req, res) => {
 	res.send({success: true})
 });
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+app.post('/', (req, res) => {
 
+	let graph = new Flow.Graph('Graph');
+	graph.init(req.body.graphs, req.body.componentClasses);
+
+	graph.execute();
+
+	res.send({ executing: true });
+
+});
+
+app.listen(3000, () => console.log('Listening on port 3000'));
