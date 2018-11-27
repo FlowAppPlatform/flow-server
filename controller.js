6002a3289ad2c11834f02d7af993b58ff0a6cd24
Mohammed Faizuddin <mdfaizuddin15@gmail.com>
2018-11-27 10:27:06 +0530

    adds a new controller file for handling the graph route
    adds cors

---
+++ controller.js
@@ -0,0 +1,53 @@
+const { Graph } = require("flow-platform-sdk");
+const componentClasses = require("./components");
+const resolveGraphs = require("./resolve-graphs");
+const api = require("./cloudboost");
+
+module.exports = (req, res, next) => {
+  // TODO: validate request data
+  const { components, inputs, outputs, appId } = req.body;
+
+  executeGraphs(req.body)
+    .then(result => {
+      res.json(result);
+    })
+    .catch(next);
+};
+
+function executeGraphs({ components, inputs, outputs, startComponent, appId }) {
+  return new Promise((resolve, reject) => {
+    api
+      // IDs to Graph Components
+      .fetchGraphComponents(appId, components)
+      // resolve dynamic properties
+      .then(graphs => {
+        return resolveGraphs({ components: graphs, inputs });
+      })
+      .then(graphs => {
+        const graph = new Graph("Graph");
+        graph.init(
+          {
+            data: [getStartComponent(startComponent), ...graphs],
+            name: "Graph"
+          },
+          componentClasses
+        );
+        graph.execute();
+        graph.onComplete(resolve);
+      })
+      .catch(reject);
+  });
+}
+
+function getStartComponent(startComponent) {
+  return {
+    id: "start",
+    graphComponentId: "start",
+    connections: [
+      {
+        fromPortId: "Start",
+        toComponentId: startComponent
+      }
+    ]
+  };
+}
--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-const flow = require('flow-platform-sdk');
-const express = require('express');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const controller = require("./controller");
+const cors = require("cors");
 
 const app = express();
 
+app.use(logger());
+app.options("*", cors());
+app.use(cors());
+
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send({success: true});
-});
+app.post("/", controller);
 
-app.post('/', (req, res) => {
-
-  let graph = new flow.Graph('Graph');
-  graph.init(req.body.graphs, req.body.componentClasses);
-
-  graph.execute();
-
-  res.send({success: true});
-
-});
-
-app.listen(3000, () => console.log('Listening on port 3000'));
+app.listen(1801, () => console.log("Listening on port 1801"));
